0bd7bbac35fad03879965480abc9aa922ba437cb
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-06-15 21:57:47 -0400

    completes Task

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,70 +1,59 @@
 package com.gildedrose;
 
 public class GildedRose {
+
+
+    private Item[] items;
+    private StrategyGildenRose  strategyGildenRose;
+
+    public GildedRose(Item[] items) {
+
+        this.items = items;
+    }
+
     public Item[] getItems() {
+
         return items;
     }
 
     public void setItems(Item[] items) {
+
         this.items = items;
     }
 
-    private Item[] items;
-
-    public GildedRose(Item[] items) {
-        this.items = items;
+    public void setEstrategyItem(StrategyGildenRose strategyGildenRose){
+        this.strategyGildenRose = strategyGildenRose ;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].getName().equals("Aged Brie")
                     && !items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].getQuality() > 0) {
-                    if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].setQuality( items[i].getQuality() - 1);
-                    }
-                }
+
+                setEstrategyItem(new ItemCriteria1());
+
             } else {
                 if (items[i].getQuality() < 50) {
-                    items[i].setQuality(items[i].getQuality() + 1);
 
-                    if (items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].getSellIn() < 11) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
+                    setEstrategyItem(new ItemCriteria2());
 
-                        if (items[i].getSellIn() < 6) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
-                    }
                 }
             }
 
+
             if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].setQuality(items[i].getSellIn() - 1);
+
+                setEstrategyItem(new ItemCriteria3());
+
             }
 
             if (items[i].getSellIn() < 0) {
-                if (!items[i].getName().equals("Aged Brie")) {
-                    if (!items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].getQuality() > 0) {
-                            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].setQuality(items[i].getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-                    }
-                } else {
-                    if (items[i].getQuality() < 50) {
-                        items[i].setQuality(items[i].getQuality() + 1);
-                    }
-                }
+
+                setEstrategyItem(new ItemCriteria4());
+
             }
+
+            strategyGildenRose.updateQuality(items[i]);
         }
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -20,10 +20,9 @@ public class GildedRoseTest {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backsthttps://github.com/Ticlla/GildedRose-Refactoring-Kata.gitage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
@@ -35,19 +34,18 @@ public class GildedRoseTest {
                 new Item("+5 Dexterity Vest", 10, 9), //
                 new Item("Aged Brie", 2, 1), //
                 new Item("Elixir of the Mongoose", 5, 4), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 14),
-                new Item("Backsthttps://github.com/Ticlla/GildedRose-Refactoring-Kata.gitage passes to a TAFKAL80ETC concert", 10, 9),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 9),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 4),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 2) };
 
 
         for (int i = 0; i < itemsEnd.length ; i++) {
-            assertEquals(itemsEnd[i].getName(),app.getItems()[i].getName());
-            assertEquals(itemsEnd[i].getQuality(),app.getItems()[i].getQuality());
-            assertEquals(itemsEnd[i].getSellIn(),app.getItems()[i].getSellIn());
+            assertEquals(String.format("%s",itemsEnd[i].getName()),itemsEnd[i].getName(),app.getItems()[i].getName());
+            assertEquals(String.format("%s",itemsEnd[i].getName()),itemsEnd[i].getQuality(),app.getItems()[i].getQuality());
+            assertEquals(String.format("%s",itemsEnd[i].getName()),itemsEnd[i].getSellIn(),app.getItems()[i].getSellIn());
         }
 
 
